5d6e0ed44864cb4355d3aac9e3e4638ca7ba3d0e
IKGapirov <IKGapirov@gmail.com>
2020-04-25 13:21:17 +0200

    hardware: Bootleg bringup

--- doze/Android.mk
+++ doze/Android.mk
@@ -16,14 +16,11 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.core_core \
     androidx.preference_preference
 
-LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform.internal
-
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
-    $(TOP)/packages/resources/devicesettings/res
+    $(LOCAL_PATH)/doze-res
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
